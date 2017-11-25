e163eae14aa5ea4f4a0876bae9f1a6d17e1dfafb
yaowalukMuz <yaowalukMuz.c@gmail.com>
2017-11-25 15:02:25 +0700

    Toolbar back step success

--- app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
+++ app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package androidthai.in.th.firebaseung.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidthai.in.th.firebaseung.MainActivity;
 import androidthai.in.th.firebaseung.R;
 
 /**
@@ -14,6 +16,45 @@ import androidthai.in.th.firebaseung.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }//Main Method
+
+    private void createToolbar() {
+
+//        Config toolbar
+        Toolbar toolbar = getView().findViewById(R.id.tbRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+
+//        Back Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//      Set onClick event
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                popBackStack มาจากหน้าไหนกลับไปหน้านั้น ==` finish
+                ((MainActivity) getActivity()).getSupportFragmentManager().popBackStack();
+        }
+        });
+
+//        IMgOnToolbar
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
